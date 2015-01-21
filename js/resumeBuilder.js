019b4db3065d51bf031f7db71480b84815e3bc5e
Tom Elliott <elliott.tj@gmail.com>
2015-01-21 17:10:36 +0300

    add .js exploring replace, append, prepend, and var. Through Lesson 1.4

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,30 @@
-$("#main").append("Thomas Elliott");
+//$("#main").append("Thomas Elliott");
+
+//variables
+var awesomeThoughts = "I am Tom and I am AWESOME!";
+
+console.log(awesomeThoughts);
+
+//test the replace function
+var email ="tj@tjelliott.com";
+var newEmail = email.replace("tjelliott","gmail");
+console.log(email);
+console.log(newEmail);
+
+//replace practice
+
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN!");
+console.log(funThoughts);
+
+$("#main").append(funThoughts);
+
+//append/replace/prepend practice
+var name = "Thomas Elliott";
+var role = "Communications Specialist";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
