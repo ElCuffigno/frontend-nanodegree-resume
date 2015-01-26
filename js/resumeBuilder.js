b3f26077463a1001ed9bbeafa3761340f1aed0e2
Tom Elliott <elliott.tj@gmail.com>
2015-01-26 14:57:18 +0300

    Add broken object example - won't show objects below appends (line 60)

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -22,22 +22,42 @@ console.log(funThoughts);
 //append/replace/prepend practice
 var name = "Thomas Elliott";
 var role = "Communications Specialist";
+var contactSkype = "elliott.tj - Skype";
+var blog = "http://www.teamcelliott.com";
+var pic = "http://www.teamcelliott.com/wp-content/uploads/2014/12/flyingbirdhead.jpg";
+var welcome = "Check out my CV";
+
 
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
 
 // arrays
 var skills =
 ["awesomeness", "skill3", "skill4"];
 console.log(skills.length);
 
+
+// objects
+var formattedContact = HTMLcontactGeneric.replace("%data%", contactSkype);
+var formattedPic = HTMLbioPic.replace("%data%", pic);
+var formattedWelcome = HTMLWelcomeMsg.replace("%data%", welcome);
+var formattedBlog = HTMLblog.replace("%data%", blog);
+
 var bio = {
-  "name" : "Thomas",
-  "age" : 29,
+  "name" : formattedName,
+  "role" : formattedRole,
+  "contact" : contactSkype,
+  "blog" : blog,
+  "picture" : pic,
+  "welcome" : welcome,
   "skills" : skills
 };
 
-$("#main").append(bio.name);
+$("#header").prepend(bio.name);
+$("#header").prepend(bio.role);
+$("#topContacts").prepend(bio.contact);
+$("#topContacts").prepend(bio.blog);
+$("#topContacts").prepend(bio.welcome);
